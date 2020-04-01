5a5f4b0c939e998218f466c75edbbef852995a37
Andrew Rodland <andrew@cleverdomain.org>
2020-04-01 00:34:48 -0400

    Basic TX support

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/arodland/pulsedax
 
 go 1.13
 
-require github.com/arodland/flexclient v0.0.0-20200326161722-e53331342ce3
+require github.com/arodland/flexclient v0.0.0-20200401032639-07e418930243
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -17,6 +21,7 @@ var cfg struct {
 	Station string
 	Slice   string
 	Sink    string
+	Source  string
 	DaxCh   string
 }
 
@@ -26,13 +31,15 @@ func init() {
 	flag.StringVar(&cfg.Slice, "slice", "A", "Slice letter to use")
 	flag.StringVar(&cfg.DaxCh, "daxch", "1", "DAX channel # to use")
 	flag.StringVar(&cfg.Sink, "sink", "flexdax.rx", "PulseAudio sink to send audio to")
+	flag.StringVar(&cfg.Source, "source", "flexdax.tx.monitor", "PulseAudio sink to receive from")
 }
 
 var fc *flexclient.FlexClient
 var ClientID string
 var ClientUUID string
 var SliceIdx string
-var StreamID string
+var RXStreamID string
+var TXStreamID string
 
 func bindClient() {
 	fmt.Println("Waiting for station:", cfg.Station)
@@ -76,12 +83,64 @@ func findSlice() {
 
 func enableDax() {
 	fc.SendAndWait("slice set " + SliceIdx + "dax=" + cfg.DaxCh)
+	fc.SendAndWait("dax audio set " + cfg.DaxCh + " slice=" + SliceIdx + " tx=1")
+
 	res := fc.SendAndWait("stream create type=dax_rx dax_channel=" + cfg.DaxCh)
 	if res.Error != 0 {
 		panic(res)
 	}
-	StreamID = res.Message
-	fmt.Println("enabled DAX stream", StreamID)
+
+	RXStreamID = res.Message
+	fmt.Println("enabled RX DAX stream", RXStreamID)
+
+	fc.SendAndWait(fmt.Sprintf("audio stream 0x%s slice %s gain %d", RXStreamID, SliceIdx, 50))
+
+	res = fc.SendAndWait("stream create type=dax_tx" + cfg.DaxCh)
+	if res.Error != 0 {
+		panic(res)
+	}
+
+	TXStreamID = res.Message
+
+	fmt.Println("enabled TX DAX stream", TXStreamID)
+}
+
+func rescale(data []byte, scale float32) []byte {
+	b := bytes.NewReader(data)
+	var out bytes.Buffer
+
+	var sample float32
+	for {
+		err := binary.Read(b, binary.BigEndian, &sample)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		binary.Write(&out, binary.BigEndian, sample*scale)
+	}
+
+	return out.Bytes()
+}
+
+func rescaleToInt(data []byte, scale float32) []byte {
+	b := bytes.NewReader(data)
+	var out bytes.Buffer
+
+	var sample float32
+	for {
+		err := binary.Read(b, binary.BigEndian, &sample)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		binary.Write(&out, binary.BigEndian, uint16(sample*scale))
+	}
+
+	return out.Bytes()
 }
 
 func streamToPulse() {
@@ -109,11 +168,56 @@ func streamToPulse() {
 	cmd.Wait()
 }
 
+func streamFromPulse() {
+	tmp, err := strconv.ParseUint(TXStreamID, 16, 32)
+	if err != nil {
+		panic(err)
+	}
+
+	StreamIDInt := uint32(tmp)
+
+	cmd := exec.Command("pacat", "-r", "--device", cfg.Source, "--rate=48000", "--channels=1", "--format=float32be", "--latency-msec=25", "/dev/stdout")
+	pipe, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	var buf [1024]byte
+	var pktCount int16
+	for {
+		n, err := io.ReadFull(pipe, buf[:])
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		//		rescaled := rescale(buf[:n], 32767)
+
+		var pkt bytes.Buffer
+		pkt.WriteByte(0x18)
+		pkt.WriteByte(0xd0 | byte(pktCount&0xf))
+		pktCount += 1
+		binary.Write(&pkt, binary.BigEndian, uint16(n/4+7))
+		binary.Write(&pkt, binary.BigEndian, StreamIDInt)
+		binary.Write(&pkt, binary.BigEndian, uint64(0x00001c2d534c03e3))
+		binary.Write(&pkt, binary.BigEndian, uint32(0x00000000))
+		binary.Write(&pkt, binary.BigEndian, uint32(0x00000000))
+		binary.Write(&pkt, binary.BigEndian, uint32(0x00000000))
+		pkt.Write(buf[:n])
+		fc.SendUdp(pkt.Bytes())
+	}
+}
+
 func main() {
 	flag.Parse()
 
 	var err error
-	fc, err = flexclient.NewFlexClient(cfg.RadioIP + ":4992")
+	fc, err = flexclient.NewFlexClient(cfg.RadioIP)
 	if err != nil {
 		panic(err)
 	}
@@ -139,7 +243,8 @@ func main() {
 	bindClient()
 	findSlice()
 	enableDax()
-	streamToPulse()
+	go streamToPulse()
+	go streamFromPulse()
 
 	wg.Wait()
 }
