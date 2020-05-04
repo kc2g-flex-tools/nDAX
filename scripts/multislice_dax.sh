0c9f9c6f168ac9df2bb1af6960c67a1f93137925
Andrew Rodland <andrew@cleverdomain.org>
2020-05-03 21:09:45 -0400

    Adjust the multi-slice example script for -tx=false usage

--- scripts/multislice_dax.sh
+++ scripts/multislice_dax.sh
@@ -4,7 +4,7 @@
 # Maestro/SmartSDR/etc. instance.
 FLEX_STATION=flex_station_name
 
-./nDAX -station ${FLEX_STATION} -slice A -daxch 1 -sink flex.sliceA.rx -source flex.sliceA.tx &
-./nDAX -station ${FLEX_STATION} -slice B -daxch 2 -sink flex.sliceB.rx -source flex.sliceB.tx &
-./nDAX -station ${FLEX_STATION} -slice C -daxch 3 -sink flex.sliceC.rx -source flex.sliceC.tx &
-./nDAX -station ${FLEX_STATION} -slice D -daxch 4 -sink flex.sliceD.rx -source flex.sliceD.tx &
+./nDAX -station ${FLEX_STATION} -slice A -daxch 1 -sink flex.sliceA.rx -source flex.tx &
+./nDAX -station ${FLEX_STATION} -slice B -daxch 2 -sink flex.sliceB.rx -tx=false &
+./nDAX -station ${FLEX_STATION} -slice C -daxch 3 -sink flex.sliceC.rx -tx=false &
+./nDAX -station ${FLEX_STATION} -slice D -daxch 4 -sink flex.sliceD.rx -tx=false &
