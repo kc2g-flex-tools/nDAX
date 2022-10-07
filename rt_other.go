0de4406c2fc237784cd3182e47158f3eba7d08ae
Andrew Rodland <andrew@cleverdomain.org>
2022-10-07 01:49:53 -0400

    Oops.

--- rt_other.go
+++ rt_other.go
@@ -1,12 +1,12 @@
 //go:build !linux
 
+package main
+
 import (
 	"runtime"
 
 	"github.com/rs/zerolog/log"
 )
-
-package main
 
 func requestRealtime(name string, _ int32) {
 	log.Debug().Str("thread", name).Str("GOOS", runtime.GOOS).Msg("realtime not (yet) supported on this OS")
